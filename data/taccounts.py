692b5cc3c9c142a0aa550b63a8352c4889432d97
Joe Harkins <joe@xmission.com>
2012-11-21 18:25:13 -0700

    Add final data from group. Add detail stuff

--- data/taccounts.py
+++ data/taccounts.py
@@ -20,13 +20,18 @@ For each account_name, create the
 '''
 
 # Used for labeling a column
-dorc = ['','', 'Debit','Credit']
+dorc = ['','', '', '', 'Debit','Credit']
 
 # Accounts dictionary
 accounts = {}
 
+# Journal Entries
+journal_entries = {} 
+
+transaction_id = False
+
 # Build a dictionary of t-accounts
-with open('journal.v2.csv', 'rb') as csvfile:
+with open('journal_entries.v3.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         # Determine if this is a blank row
@@ -38,43 +43,68 @@ with open('journal.v2.csv', 'rb') as csvfile:
         # Keep track of the account
         account = ''
 
+        # Keep track of transaction for the journal
+
         for cell in row:
             if cell != '':
                 all_blank = False
 
                 # These define a transaction - might be important later
-                if re.match('^[0-9]', cell):
-                    print "Transaction Detected: {}".format(cell)
+                if re.match('^[0-9]+$', cell):
+                    print "\nTransaction Detected: {}".format(cell)
+                    transaction_id = cell
+                    journal_entries[transaction_id] = {
+                            'id': cell,
+                            'date': '31-Dec',
+                            'transaction_note': '',
+                            'entries': []
+                    }
 
-                if re.match('^[A-Za-z-_\(\)]', cell):
+                if re.match('^[0-9]+-[A-Za-z]+$', cell):
+                    print "Date Detected: {}".format(cell)
+                    journal_entries[transaction_id]['date'] = cell
+
+                if re.match('^[\sA-Za-z-_\(\)]', cell):
                     a = cell.strip()
-                    print "Account Detected: {}".format(a)
-                    account = a
+                    if cn == 2:
+                        print "Note Detected: {} in column {}".format(a,cn)
+                        journal_entries[transaction_id]['transaction_note'] = a
+
+                    if cn == 3:
+                        print "Account Detected: {} in column {}".format(a,cn)
+                        account = a
 
                     # Add the account to the dict
                     if account not in accounts:
                         # Frame out the account
                         accounts[account] = {'Debit': [], 'Credit': []}
 
-                if re.match('^[\$]', cell):
+                if re.search('\$[\d,\.]+', cell) and (cn == 4 or cn == 5):
                     s = re.sub(r'\s', '', cell)
 
                     if account == '':
+                        print row
                         raise Exception('A transaction without an account!')
                     
-                    print "{} Detected: {}".format(dorc[cn], s)
-                    accounts[account][dorc[cn]].append(s)
+                    print "In {} a {} of {} was found".format(account, dorc[cn], s)
+
+                    accounts[account][dorc[cn]].append([transaction_id, s])
+                    journal_entries[transaction_id]['entries'].append( [account, dorc[cn], s])
 
             cn+=1
+
 
         # If it's not a blank row, 
         if all_blank == False:
             pass
 
-
+pprint(journal_entries)
 pprint(accounts)
 
-with io.open('taccounts.json', 'wb') as outfile:
+with io.open('taccounts.v4.json', 'wb') as outfile:
     json.dump(accounts, outfile)
+
+with io.open('journal_entries.v4.json', 'wb') as outfile:
+    json.dump(journal_entries, outfile)
 
 
